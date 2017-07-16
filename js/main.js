7ee8619f7ca2ffde088ee8f9af7e7dc2eb98fbb4
claucvk <claudiacv22@gmail.com>
2017-07-16 11:49:48 -0400

    card array of objects, update checkForMatch and flipCard functions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 console.log('Up and running!');
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png',
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png',
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png',
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png',
+  },
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
   if (cardsInPlay.length === 2) {
-    if (cards[cardsInPlay[0]] === cards[cardsInPlay[1]]) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
       alert('You found a match!');
     } else {
       alert('Sorry, try again.');
@@ -14,7 +36,9 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-  cardsInPlay.push([cardId]);
-  console.log('User flipped ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log('User flipped ' + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   checkForMatch();
 };

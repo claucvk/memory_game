c4c0655371a5944611a161dc32344dfeb05fbed0
claucvk <claudiacv22@gmail.com>
2017-07-17 09:02:30 -0400

    final touches of the game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 console.log('Up and running!');
 
-var cards = [
-  {
+var cards = [{
     rank: 'queen',
     suit: 'hearts',
     cardImage: 'images/queen-of-hearts.png',
@@ -25,7 +24,8 @@ var cards = [
 
 var cardsInPlay = [];
 
-var checkForMatch = function () {
+var checkForMatch = function() {
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert('You found a match!');
@@ -35,10 +35,26 @@ var checkForMatch = function () {
   }
 };
 
-var flipCard = function (cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardId].rank);
   console.log('User flipped ' + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   checkForMatch();
 };
+
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', function () {
+      flipCard();
+    });
+
+    document.getElementsById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard();
